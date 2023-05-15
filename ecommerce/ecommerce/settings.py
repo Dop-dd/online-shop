7aa37c812174f8de3dc7dafba8f8f94beee64179
Dop <dopddaiga@gmail.com>
2023-05-15 21:35:36 +0200

    Added models to Admin page

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -117,7 +117,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# connct the statit folder
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# upload media files
+MEDIA_URL = '/media/'
+
+# images
+MEDIA_ROOT = BASE_DIR / 'static/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from . models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # pre-populate the fields
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    # pre-populate the fields
+    prepopulated_fields = {'slug': ('title',)}
