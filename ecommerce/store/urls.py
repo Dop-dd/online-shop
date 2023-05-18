15487619c6c9163cd59461451dc4b70a0dc3fb23
Dop <dopddaiga@gmail.com>
2023-05-18 14:48:59 +0200

    Added the main page

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.store, name='store'),
+    path('product/<slug:slug>/', views.product_info, name='product_info'),
 
     ]
