1ca68af23b00e789195258cb820eb3619f026f69
Dop <dopddaiga@gmail.com>
2023-05-19 13:19:52 +0200

    Added the Shopping cart

---
+++ ecommerce/cart/cart.py
@@ -0,0 +1,4 @@
+# a session for our shopping app
+
+# class Cart():
+#     def __init__(self, request):
--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'store', # django app
+    'cart', # django app
 ]
 
 MIDDLEWARE = [
