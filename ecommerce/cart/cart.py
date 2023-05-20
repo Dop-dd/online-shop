0e1f426ee7ec87f0da1f237fc5c67f67c1ae400f
Dop <dopddaiga@gmail.com>
2023-05-20 07:28:38 +0200

    Added Ajax integration and created view for cart add

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -1,4 +1,25 @@
 # a session for our shopping app
 
-# class Cart():
-#     def __init__(self, request):
+class Cart():
+
+    def __init__(self, request):
+        self.session = request.session
+        # returning user, retrieve their existing session key
+        cart = self.session.get('session_key')
+
+        # if user is new, ceates a new session key
+        if 'session_key' not in request.session:
+            cart = self.session['session_key'] = {}
+
+        self.cart = cart
+
+    def add(self, product, product_qty):
+        product_id = str(product.id)
+
+        if product_id in self.cart:
+            self.cart[product_id]['qty'] = product_qty
+        else:
+            self.cart[product_id] = {'price': str(product.price), 'qty': product_qty}
+
+        self.session.modified = True
+
--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-#from . models import Category, Product
-#from django.shortcuts import get_object_or_404
+
+# import the Cart view
+from .cart import Cart
+from store.models import Product
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
@@ -9,7 +12,16 @@ def cart_summary(request):
     return render(request, 'cart/cart-summary.html')
 
 def cart_add(request):
-    pass
+    # grab the ajax code we created to add item quantity
+    cart = Cart(request)
+    if request.POST.get('action') == 'POST':
+        product_id = int(request.POST.get('product_id'))
+        product_quantity = int(request.POST.get('product_quantity'))
+
+        # find the id in the DB (import the product model)
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product, product_qty=product_quantity)
+
 
 def cart_delete(request):
     pass
