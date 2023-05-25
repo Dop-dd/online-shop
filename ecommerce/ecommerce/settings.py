74b0570ae07e42719fa2f492160008333770b42f
Dop <dopddaiga@gmail.com>
2023-05-25 20:11:19 +0200

    Added Email backend and app password.

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -140,5 +140,11 @@ MEDIA_ROOT = BASE_DIR / 'static/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = 'True'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST_USER = 'thingscommute@gmail.com'
+EMAIL_HOST_PASSWORD = 'mxzqqhwjtrrdlgav'
