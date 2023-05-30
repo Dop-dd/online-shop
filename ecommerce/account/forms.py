d932b8718c634af14776cf8fb03e3e2cde41beac
Dop <dopddaiga@gmail.com>
2023-05-30 20:10:14 +0200

    Added template to update username and email.

--- ecommerce/account/forms.py
+++ ecommerce/account/forms.py
@@ -38,3 +38,21 @@ class LoginForm(AuthenticationForm):
 
     username = forms.CharField(widget=TextInput())
     password = forms.CharField(widget=PasswordInput())
+
+
+# update forms
+class UpdateUserForm(forms.ModelForm):
+
+    password = None
+
+     # make emails unique
+    def __init__(self, *args, **kwargs):
+        super(UpdateUserForm, self).__init__(*args, **kwargs)
+
+        # mark email field as required
+        self.fields['email'].required = True
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+        exclude = ['password', 'password']
